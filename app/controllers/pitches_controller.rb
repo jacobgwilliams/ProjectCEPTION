21bcbb5356826d4a6ce83abb98772fdb3844a9f6
Lindsay Kelly <email": null>
2016-11-18 17:27:23 -0500

    Add pitch create functionality

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -5,13 +5,31 @@ class PitchesController < ApplicationController
   end
 
   def new
+    @pitch = Pitch.new
   end
 
   def create
+    @pitch = Pitch.new(pitch_params)
+    if current_user.pitches.length < 4
+      if @pitch.save
+        flash[:notice] = "Thank you for adding a new pitch! Don't forget to vote!"
+        redirect_to root_path #????
+      else
+        render 'new'
+      end
+    else
+      flash[:notice] = "User have too much pitches for user"
+      redirect_to root_path
+    end
   end
 
   def show
     @pitch = Pitch.find_by(id: params[:id])
   end
+
+  private
+    def pitch_params
+      params.require(:pitch).permit(:title, :body)
+    end
 
 end
