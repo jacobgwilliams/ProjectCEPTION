a0ab286566d639af03acbec6b99eb241846ea54c
Jacob Williams <email": null>
2016-11-20 12:35:30 -0500

    Final Group Selection from dashboard 1. changes 'choose as final project' link to a 'already chosen' message if chosen, and 2. path is protected and will redirect to index if force navigated to

--- app/controllers/final_groups_controller.rb
+++ app/controllers/final_groups_controller.rb
@@ -7,6 +7,7 @@ class FinalGroupsController < ApplicationController
   def new
     require_login
     require_admin
+    final_group_protection
     @final_group = FinalGroup.new(pitch_id: params[:pitch_id])
     @students = User.where(admin: false)
   end
@@ -20,6 +21,16 @@ class FinalGroupsController < ApplicationController
     end
     flash[:notice] = 'That was a great group you just created'
     redirect_to current_user
+  end
+
+  def final_group_created(pitch)
+    pitch.final_group
+  end
+
+  def final_group_protection
+    if final_group_created(Pitch.find_by(id: params[:pitch_id]))
+      redirect_to '/'
+    end
   end
 
 end
