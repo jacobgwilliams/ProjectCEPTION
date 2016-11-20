71096f83d92080e277375aa0ff8229648bbb1769
Jacob Williams <email": null>
2016-11-20 13:36:36 -0500

    1. Students added to a final group cannot be added to another. 2. Team leaders are excluded from final group selection form. 3. A notice is displayed in round 2 voting page if a pitch's owner is already a team lead on a decided final group.

--- app/controllers/final_groups_controller.rb
+++ app/controllers/final_groups_controller.rb
@@ -7,9 +7,9 @@ class FinalGroupsController < ApplicationController
   def new
     require_login
     require_admin
-    final_group_protection
+    final_group_protection #will currently redirect admin to '/' if the pitch has already been made into a final group
     @final_group = FinalGroup.new(pitch_id: params[:pitch_id])
-    @students = User.where(admin: false)
+    @students = final_group_selection_listing
   end
 
   def create_groups
@@ -31,6 +31,20 @@ class FinalGroupsController < ApplicationController
     if final_group_created(Pitch.find_by(id: params[:pitch_id]))
       redirect_to '/'
     end
+  end
+
+  def is_group_leader?(student, pitch)
+    pitch.student == student ? true : false
+  end
+
+  def leaders_already
+    FinalGroup.all.map(&:leader)
+  end
+
+  def final_group_selection_listing
+    users = User.where(admin: false)
+    users = users.find_all {|user| user.final_group == nil }
+    users = users.delete_if {|user| leaders_already.include?(user) }
   end
 
 end
--- app/helpers/final_groups_helper.rb
+++ app/helpers/final_groups_helper.rb
@@ -2,4 +2,8 @@ module FinalGroupsHelper
   def is_group_leader?(student, pitch)
     pitch.student == student ? true : false
   end
+
+  def leaders_already
+    FinalGroup.all.map(&:leader)
+  end
 end
